d54211acc005b8b937bdf0d826c0e1f017268f88
cgrube20 <cgrube20@gmail.com>
2021-08-05 19:26:00 -0500

    Final Version PA 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## For makeCacheMatrix
+## inv represents null objects, spec allows me to specify the value of the matrix,
+## The first two <<- functions allow the variables to change in the parent levels
+## pull gets the value of the matrix
+## specInverse specifies the value of inverse, <<- is needed again
+## pullInverse gets the value out
+##
+## For cacheSolve
+## inv is used for to pull the inverse
+## if leads to the return function is criteria are met
+## mat pulls the matrix
+##I tested using a 2 by 2 pmatrix function
 
-## Write a short comment describing this function
+## makeCacheMatrix will create and store our initial matrix values for later calculation
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        spec <- function(y){
+                x <<- y
+                inv <<- NULL
+        }
+        pull <- function() {x}
+        specInverse <- function(inverse) {inv <<- inverse}
+        pullInverse <- function() {inv}
+        list(spec = spec, pull = pull, specInverse = specInverse, pullInverse = pullInverse)
 }
 
 
-## Write a short comment describing this function
+## This function will return the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$pullInverse()
+        if(!is.null(inv)){
+                message("pulling cache data")
+                return(inv)
+        }
+        mat <- x$pull()
+        inv <- solve(mat, ...)
+        x$specInverse(inv)
+        inv
 }
